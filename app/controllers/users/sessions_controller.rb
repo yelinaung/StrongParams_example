f5b488a8e4520c31f738eb5ea3f4fb8bf22984ad
mgpyone <mgpyone@mysteryzillion.com>
2013-06-29 09:09:04 +0630

    user can sign with 'name' now!

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,4 @@
-class Users::SessionsController < Devise::SessionsController
+class Users::RegistrationsController < Devise::RegistrationsController
   def sign_in_params
     params.require(:user).permit(:name, :email, :password, :remember_me )
   end
